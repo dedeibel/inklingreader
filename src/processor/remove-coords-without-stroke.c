e29ac3b76f2c6d11044f4c8c45a75eee6eae0455
Benjamin Peter <dedeibel@arcor.de>
2017-09-19 22:39:56 +0200

    Added removal of coords, tilt and pressure if they are not inside of a stroke block

--- src/processor/remove-coords-without-stroke.c
+++ src/processor/remove-coords-without-stroke.c
@@ -19,12 +19,72 @@
 
 #include "remove-coords-without-stroke.h"
 
+#include <stdio.h>
 #include "../datatypes/element.h"
 #include "../datatypes/coordinate.h"
-#include "../datatypes/clock.h"
+
+void delete_entries(GSList** data, GSList** coord_list_entries, unsigned int inside_stroke)
+{
+  if (*coord_list_entries == NULL) {
+    return;
+  }
+
+  if (! inside_stroke) { 
+    unsigned int removed = 0;
+    GSList* coord_list_it = (*coord_list_entries)->next;
+    while (coord_list_it != NULL)
+    {
+      *data = g_slist_delete_link(*data, coord_list_it->data);
+      coord_list_it = coord_list_it->next;
+      removed++;
+    }
+
+    if (removed)
+      printf("  deleted %hd elements out of strokes\n", removed);
+  }
+
+  g_slist_free(*coord_list_entries);
+  *coord_list_entries = NULL;
+}
 
 GSList* pro_remove_coords_without_stroke (GSList* data)
 {
-  GSList* head = data;
-  return head;
+  GSList* it = data;
+  GSList* coord_list_entries = NULL;
+  unsigned char inside_stroke = 0;
+
+  while (it != NULL)
+  {
+    dt_element* e = (dt_element *)it->data;
+
+    if (e->type == TYPE_COORDINATE) {
+      if (! inside_stroke) {
+        coord_list_entries = g_slist_append(coord_list_entries, it);
+      }
+    }
+    if (e->type == TYPE_PRESSURE) {
+      if (! inside_stroke) {
+        coord_list_entries = g_slist_append(coord_list_entries, it);
+      }
+    }
+    if (e->type == TYPE_TILT) {
+      if (! inside_stroke) {
+        coord_list_entries = g_slist_append(coord_list_entries, it);
+      }
+    }
+    else if (e->type == TYPE_STROKE) {
+      dt_stroke* s = (dt_stroke *)e;
+
+      delete_entries(&data, &coord_list_entries, inside_stroke);
+
+      inside_stroke = s->value;
+    }
+
+    it = it->next;
+  }
+
+  delete_entries(&data, &coord_list_entries, inside_stroke);
+
+  return data;
 }
+
