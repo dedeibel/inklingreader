8bb7af30043792b77d98fe41e6e16c410b86ea19
Benjamin Peter <dedeibel@arcor.de>
2017-10-19 22:45:58 +0200

    implemented emove-duplicate-coords

--- src/optimizers/remove-duplicate-coords.c
+++ src/optimizers/remove-duplicate-coords.c
@@ -19,12 +19,74 @@
 
 #include "remove-duplicate-coords.h"
 
+#include <stdio.h>
 #include "../datatypes/element.h"
 #include "../datatypes/coordinate.h"
-#include "../datatypes/clock.h"
+
+void remove_list_entries(GSList** data, GSList** coord_list_entries)
+{
+  GSList* coord_list_it = (*coord_list_entries);
+  while (coord_list_it != NULL)
+  {
+      GSList* link_to_be_removed = coord_list_it->data;
+      g_free(link_to_be_removed->data);
+      *data = g_slist_delete_link(*data, link_to_be_removed);
+    coord_list_it = coord_list_it->next;
+  }
+
+  g_slist_free(*coord_list_entries);
+  *coord_list_entries = NULL;
+}
+
+int is_equal_coordinate(dt_coordinate* a, dt_coordinate* b) {
+  return
+    a->x == b->x &&
+    a->y == b->y;
+}
 
 GSList* opt_remove_duplicate_coords (GSList* data)
 {
-  GSList* head = data;
-  return head;
+  GSList* it = data;
+  int found_duplicate = 0;
+  GSList* coord_list_entries = NULL;
+  dt_coordinate* prev_coordinate = NULL;
+
+  while (it != NULL)
+  {
+    dt_element* e = (dt_element *)it->data;
+    if (prev_coordinate == NULL) {
+      if (e->type == TYPE_COORDINATE) {
+        prev_coordinate = (dt_coordinate*) e;
+      }
+    }
+    else {
+      if (e->type == TYPE_COORDINATE) {
+        if (is_equal_coordinate(prev_coordinate, (dt_coordinate*)e)) {
+          found_duplicate = 1;
+          coord_list_entries = g_slist_append(coord_list_entries, it);
+        }
+        else {
+          found_duplicate = 0;
+          prev_coordinate = (dt_coordinate*)e;
+          remove_list_entries(&data, &coord_list_entries);
+        }
+      }
+      else if (found_duplicate && (e->type == TYPE_TILT || e->type == TYPE_PRESSURE)) {
+        coord_list_entries = g_slist_append(coord_list_entries, it);
+      }
+      else if (e->type == TYPE_STROKE) {
+        dt_stroke* s = (dt_stroke *)e;
+        if (! s->value) {
+          found_duplicate = 0;
+          prev_coordinate = NULL;
+          remove_list_entries(&data, &coord_list_entries);
+        }
+      }
+    }
+
+    it = it->next;
+  }
+
+  remove_list_entries(&data, &coord_list_entries);
+  return data;
 }
